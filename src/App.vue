fc85e0e3ebba38b3673b79f4720faff31075225b
davidavila2 <david.avila@briebug.com>
2022-01-21 22:24:31 -0700

    implement a basic todo app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="text" name="title" id="title" v-model="todo" placeholder="add todo...">
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id">{{todo.title}} <button v-on:click="deleteTodo(todo.id)">X</button></li>
+    </ul>
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
+      <button style="width: 100px; margin-right: 10px;" @click="addTodo">Add Todo</button>
+      <button style="width: 100px; margin-left: 10px" @click="resetTodos">Reset Todos</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todo: '',
+      todos: [
+        {
+          id: Math.random(),
+          title: 'this is a todo',
+        },
+        {
+          id: Math.random(),
+          title: 'this is a todo too',
+        },
+      ],
+      deleteTodo(id) {
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+      },
+      addTodo() {
+        if(this.todo.length === 0) return;
+
+        this.todos.push({ id: Math.random(), title: this.todo });
+        this.todo = '';
+      },
+      resetTodos() {
+        this.todos = [];
+      }
+    };
+  },
+};
 </script>
 
 <style>
